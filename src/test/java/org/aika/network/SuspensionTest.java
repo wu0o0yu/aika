92a6032ba92d453c343df1ef714cf0e0e6d37093
Lukas Molzberger <lukas.molzberger@meinestadt.de>
2017-08-20 09:20:19 +0200

    work on a suspension test case

--- src/test/java/org/aika/network/SuspensionTest.java
+++ src/test/java/org/aika/network/SuspensionTest.java
@@ -17,14 +17,20 @@
 package org.aika.network;
 
 
+import org.aika.Input;
 import org.aika.Model;
 import org.aika.SuspensionHook;
 import org.aika.corpus.Document;
+import org.aika.corpus.Range;
 import org.aika.neuron.InputNeuron;
+import org.aika.neuron.Neuron;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Map;
 import java.util.TreeMap;
+
+import static org.aika.corpus.Range.Operator.EQUALS;
 
 /**
  *
@@ -46,6 +52,66 @@ public class SuspensionTest {
         Document doc = m.createDocument("Bla");
         n.addInput(doc, 0, 1);
     }
+
+
+    @Test
+    public void testSuspendAndNeuron() {
+        Model m = new Model();
+        m.suspensionHook = new DummySuspensionHook();
+
+        InputNeuron inA = m.createOrLookupInputNeuron("A");
+        InputNeuron inB = m.createOrLookupInputNeuron("B");
+
+        Neuron nC = m.createAndNeuron(new Neuron("C"), 0.5,
+                new Input()
+                        .setNeuron(inA)
+                        .setWeight(10.0)
+                        .setMinInput(0.9)
+                        .setRelativeRid(0)
+                        .setRecurrent(false)
+                        .setStartRangeMatch(EQUALS)
+                        .setStartRangeOutput(true),
+                new Input()
+                        .setNeuron(inB)
+                        .setWeight(10.0)
+                        .setMinInput(0.9)
+                        .setRelativeRid(null)
+                        .setRecurrent(false)
+                        .setEndRangeMatch(EQUALS)
+                        .setEndRangeOutput(true)
+        );
+
+
+        Neuron outD = m.createAndNeuron(new Neuron("D"), 0.5,
+                new Input()
+                        .setNeuron(nC)
+                        .setWeight(10.0)
+                        .setMinInput(0.9)
+                        .setRecurrent(false)
+                        .setRangeMatch(Input.RangeRelation.EQUALS)
+                        .setRangeOutput(true)
+        );
+
+
+//        inA.suspend(m);
+//        inB.suspend(m);
+
+
+        // Reactivate
+
+        Document doc = m.createDocument("Bla");
+
+        inA = m.createOrLookupInputNeuron("A");
+        inA.addInput(doc, 0, 1, 0);
+
+        inB = m.createOrLookupInputNeuron("B");
+        inB.addInput(doc, 1, 2, 1);
+
+        doc.process();
+
+        Assert.assertFalse(outD.getFinalActivations(doc).isEmpty());
+    }
+
 
 
 
