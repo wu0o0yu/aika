8429cae0cca0ff58d49a13bfa72aa8d021b9719e
Lukas Molzberger <lukas.molzberger@meinestadt.de>
2017-10-10 09:01:46 +0200

    adjust test case

---
+++ src/test/java/org/aika/network/AdjustTest.java
@@ -0,0 +1,94 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.aika.network;
+
+import org.aika.Input;
+import org.aika.Model;
+import org.aika.Neuron;
+import org.aika.corpus.Range;
+import org.aika.lattice.Node;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Lukas Molzberger
+ */
+public class AdjustTest {
+
+
+    @Test
+    public void testAdjust() {
+        Model m = new Model();
+
+        Neuron inA = m.createNeuron("A");
+        Neuron inB = m.createNeuron("B");
+        Neuron inC = m.createNeuron("C");
+        Neuron inD = m.createNeuron("D");
+
+        Neuron out = m.initNeuron(m.createNeuron("ABCD"),
+                -9.5,
+                new Input()
+                        .setNeuron(inA)
+                        .setWeight(4.0f)
+                        .setRecurrent(false)
+                        .setBiasDelta(0.0)
+                        .setStartRangeMatch(Range.Operator.EQUALS)
+                        .setStartRangeOutput(true)
+                        .setEndRangeMatch(Range.Operator.EQUALS)
+                        .setEndRangeOutput(true),
+                new Input()
+                        .setNeuron(inB)
+                        .setWeight(3.0f)
+                        .setRecurrent(false)
+                        .setBiasDelta(0.0)
+                        .setStartRangeMatch(Range.Operator.EQUALS)
+                        .setStartRangeOutput(true)
+                        .setEndRangeMatch(Range.Operator.EQUALS)
+                        .setEndRangeOutput(true),
+                new Input()
+                        .setNeuron(inC)
+                        .setWeight(2.0f)
+                        .setRecurrent(false)
+                        .setBiasDelta(0.0)
+                        .setStartRangeMatch(Range.Operator.EQUALS)
+                        .setStartRangeOutput(true)
+                        .setEndRangeMatch(Range.Operator.EQUALS)
+                        .setEndRangeOutput(true),
+                new Input()
+                        .setNeuron(inD)
+                        .setWeight(1.0f)
+                        .setRecurrent(false)
+                        .setBiasDelta(0.0)
+                        .setStartRangeMatch(Range.Operator.EQUALS)
+                        .setStartRangeOutput(true)
+                        .setEndRangeMatch(Range.Operator.EQUALS)
+                        .setEndRangeOutput(true)
+        );
+
+        System.out.println(out.get().node.get().logicToString());
+
+        out.get().bias = -8.5;
+        Node.adjust(m, 0, out.get(), -1, out.get().inputSynapses.values());
+
+        System.out.println(out.get().node.get().logicToString());
+
+        Assert.assertEquals(1, out.get().node.get().parents.firstEntry().getValue().size());
+    }
+}
