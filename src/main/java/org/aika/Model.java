cdb88f2b43800f247bdd542136975563e7e63064
Lukas Molzberger <lukas.molzberger@meinestadt.de>
2018-01-17 13:16:47 +0100

    fix

--- src/main/java/org/aika/Model.java
+++ src/main/java/org/aika/Model.java
@@ -255,7 +255,7 @@ public class Model {
 
     private boolean suspend(int docId, Provider<? extends AbstractNode> p, SuspensionMode sm) {
         AbstractNode an = p.getIfNotSuspended();
-        if (an != null && an.lastUsedDocumentId <= docId) {
+        if (an != null && an.lastUsedDocumentId < docId) {
             p.suspend(sm);
             return true;
         }
