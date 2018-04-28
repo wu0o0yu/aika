61ba48c534074aeaeae995c8e62bdab0e57f51b1
Lukas Molzberger <lukas.molzberger@meinestadt.de>
2018-04-28 11:09:54 +0200

    fixed test case

--- src/test/java/network/aika/lattice/PatternLatticeTest.java
+++ src/test/java/network/aika/lattice/PatternLatticeTest.java
@@ -39,219 +39,55 @@ public class PatternLatticeTest {
         Neuron inA = m.createNeuron("A");
         Neuron inB = m.createNeuron("B");
         Neuron inC = m.createNeuron("C");
-        Neuron inD = m.createNeuron("D");
 
-        {
-            Neuron.init(m.createNeuron("ABC"),
-                    0.001,
-                    INeuron.Type.EXCITATORY,
-                    new Synapse.Builder()
-                            .setSynapseId(0)
-                            .setNeuron(inA)
-                            .setWeight(1.0)
-                            .setRecurrent(false)
-                            .setBias(-1.0)
-                            .addRangeRelation(Relation.EQUALS, 1)
-                            .setRangeOutput(true),
-                    new Synapse.Builder()
-                            .setSynapseId(1)
-                            .setNeuron(inB)
-                            .setWeight(1.0)
-                            .setRecurrent(false)
-                            .setBias(-1.0)
-                            .addRangeRelation(Relation.EQUALS, 2)
-                            .setRangeOutput(true),
-                    new Synapse.Builder()
-                            .setSynapseId(2)
-                            .setNeuron(inC)
-                            .setWeight(1.0)
-                            .setRecurrent(false)
-                            .setBias(-1.0)
-                            .setRangeOutput(true)
-            );
+        Neuron.init(m.createNeuron("ABC"),
+                0.001,
+                INeuron.Type.EXCITATORY,
+                new Synapse.Builder()
+                        .setSynapseId(0)
+                        .setNeuron(inA)
+                        .setWeight(1.0)
+                        .setRecurrent(false)
+                        .setBias(-1.0)
+                        .addRangeRelation(Relation.EQUALS, 1)
+                        .setRangeOutput(true),
+                new Synapse.Builder()
+                        .setSynapseId(1)
+                        .setNeuron(inB)
+                        .setWeight(1.0)
+                        .setRecurrent(false)
+                        .setBias(-1.0)
+                        .addRangeRelation(Relation.EQUALS, 2)
+                        .setRangeOutput(true),
+                new Synapse.Builder()
+                        .setSynapseId(2)
+                        .setNeuron(inC)
+                        .setWeight(1.0)
+                        .setRecurrent(false)
+                        .setBias(-1.0)
+                        .setRangeOutput(true)
+        );
 
-            Document doc = m.createDocument(null, 0);
+        Document doc = m.createDocument(null, 0);
 
 
-            InputNode pA = inA.get().outputNode.get();
-            InputNode pB = inB.get().outputNode.get();
-            InputNode pC = inC.get().outputNode.get();
+        InputNode pA = inA.get().outputNode.get();
+        InputNode pB = inB.get().outputNode.get();
+        InputNode pC = inC.get().outputNode.get();
 
-            AndNode pAB = pA.andChildren.get(new Refinement(null, pB.provider)).child.get();
-            Assert.assertEquals(pAB.provider, pB.andChildren.get(new Refinement(null, pA.provider)));
+        AndNode pAB = pA.andChildren.firstEntry().getValue().child.get();
+        Assert.assertNotNull(pAB.provider);
 
-            AndNode pAC = pA.andChildren.get(new Refinement(null, pC.provider)).child.get();
-            Assert.assertEquals(pAC.provider, pC.andChildren.get(new Refinement(null, pA.provider)));
+        AndNode pBC = pC.andChildren.firstEntry().getValue().child.get();
+        Assert.assertNotNull(pBC.provider);
 
-            AndNode pBC = pB.andChildren.get(new Refinement(null, pC.provider)).child.get();
-            Assert.assertEquals(pBC.provider, pC.andChildren.get(new Refinement(null, pB.provider)));
+        Assert.assertEquals(1, pAB.andChildren.size());
+        Assert.assertEquals(1, pBC.andChildren.size());
 
-            Assert.assertEquals(1, pAB.andChildren.size());
-            Assert.assertEquals(1, pBC.andChildren.size());
-            Assert.assertEquals(1, pAC.andChildren.size());
+        AndNode pABC = pAB.andChildren.firstEntry().getValue().child.get();
+        Assert.assertNotNull(pABC);
 
-            AndNode pABC = pAB.andChildren.get(new Refinement(null, pC.provider)).child.get();
-            Assert.assertEquals(pABC.provider, pAC.andChildren.get(new Refinement(null, pB.provider)));
-            Assert.assertEquals(pABC.provider, pBC.andChildren.get(new Refinement(null, pA.provider)));
-
-            Assert.assertEquals(3, pABC.parents.size());
-            Assert.assertEquals(pAB.provider, pABC.parents.get(new Refinement(null, pC.provider)));
-            Assert.assertEquals(pAC.provider, pABC.parents.get(new Refinement(null, pB.provider)));
-            Assert.assertEquals(pBC.provider, pABC.parents.get(new Refinement(null, pA.provider)));
-        }
-        {
-            Neuron.init(m.createNeuron("BCD"),
-                    0.001,
-                    INeuron.Type.EXCITATORY,
-                    new Synapse.Builder()
-                            .setSynapseId(0)
-                            .setNeuron(inB)
-                            .setWeight(1.0)
-                            .setRecurrent(false)
-                            .setBias(-1.0)
-                            .addRangeRelation(Relation.EQUALS, 1)
-                            .setRangeOutput(true),
-                    new Synapse.Builder()
-                            .setSynapseId(1)
-                            .setNeuron(inC)
-                            .setWeight(1.0)
-                            .setRecurrent(false)
-                            .setBias(-1.0)
-                            .addRangeRelation(Relation.EQUALS, 2)
-                            .setRangeOutput(true),
-                    new Synapse.Builder()
-                            .setSynapseId(2)
-                            .setNeuron(inD)
-                            .setWeight(1.0)
-                            .setRecurrent(false)
-                            .setBias(-1.0)
-                            .setRangeOutput(true)
-            );
-
-            Document doc = m.createDocument(null, 0);
-
-            InputNode pA = inA.get().outputNode.get();
-            InputNode pB = inB.get().outputNode.get();
-            InputNode pC = inC.get().outputNode.get();
-            InputNode pD = inC.get().outputNode.get();
-
-            AndNode pAB = pA.andChildren.get(new Refinement(null, pB.provider)).child.get();
-            Assert.assertEquals(pAB.provider, pB.andChildren.get(new Refinement(null, pA.provider)));
-
-            AndNode pAC = pA.andChildren.get(new Refinement(null, pC.provider)).child.get();
-            Assert.assertEquals(pAC.provider, pC.andChildren.get(new Refinement(null, pA.provider)));
-
-            AndNode pBC = pB.andChildren.get(new Refinement(null, pC.provider)).child.get();
-            Assert.assertEquals(pBC.provider, pC.andChildren.get(new Refinement(null, pB.provider)));
-
-            AndNode pBD = pB.andChildren.get(new Refinement(null, pD.provider)).child.get();
-            Assert.assertEquals(pBD.provider, pD.andChildren.get(new Refinement(null, pB.provider)));
-
-            AndNode pCD = pC.andChildren.get(new Refinement(null, pD.provider)).child.get();
-            Assert.assertEquals(pCD.provider, pD.andChildren.get(new Refinement(null, pC.provider)));
-
-
-            Assert.assertEquals(1, pAB.andChildren.size());
-            Assert.assertEquals(1, pAC.andChildren.size());
-            Assert.assertEquals(2, pBC.andChildren.size());
-            Assert.assertEquals(1, pBD.andChildren.size());
-            Assert.assertEquals(1, pCD.andChildren.size());
-
-            AndNode pBCD = pBC.andChildren.get(new Refinement(null, pD.provider)).child.get();
-            Assert.assertEquals(pBCD.provider, pBD.andChildren.get(new Refinement(null, pC.provider)));
-            Assert.assertEquals(pBCD.provider, pCD.andChildren.get(new Refinement(null, pB.provider)));
-
-            Assert.assertEquals(3, pBCD.parents.size());
-            Assert.assertEquals(pBC.provider, pBCD.parents.get(new Refinement(null, pD.provider)));
-            Assert.assertEquals(pBD.provider, pBCD.parents.get(new Refinement(null, pC.provider)));
-            Assert.assertEquals(pCD.provider, pBCD.parents.get(new Refinement(null, pB.provider)));
-        }
-        {
-            Neuron.init(m.createNeuron("ABCD"),
-                    0.001,
-                    INeuron.Type.EXCITATORY,
-                    new Synapse.Builder()
-                            .setSynapseId(0)
-                            .setNeuron(inA)
-                            .setWeight(1.0)
-                            .setRecurrent(false)
-                            .setBias(-1.0)
-                            .addRangeRelation(Relation.EQUALS, 1)
-                            .setRangeOutput(true),
-                    new Synapse.Builder()
-                            .setSynapseId(1)
-                            .setNeuron(inB)
-                            .setWeight(1.0)
-                            .setRecurrent(false)
-                            .setBias(-1.0)
-                            .addRangeRelation(Relation.EQUALS, 2)
-                            .setRangeOutput(true),
-                    new Synapse.Builder()
-                            .setSynapseId(2)
-                            .setNeuron(inC)
-                            .setWeight(1.0)
-                            .setRecurrent(false)
-                            .setBias(-1.0)
-                            .addRangeRelation(Relation.EQUALS, 3)
-                            .setRangeOutput(true),
-                    new Synapse.Builder()
-                            .setSynapseId(3)
-                            .setNeuron(inD)
-                            .setWeight(1.0)
-                            .setRecurrent(false)
-                            .setBias(-1.0)
-                            .setRangeOutput(true)
-            );
-
-            Document doc = m.createDocument(null, 0);
-
-            InputNode pA = inA.get().outputNode.get();
-            InputNode pB = inB.get().outputNode.get();
-            InputNode pC = inC.get().outputNode.get();
-            InputNode pD = inC.get().outputNode.get();
-
-            AndNode pAD = pA.andChildren.get(new Refinement(null, pD.provider)).child.get();
-            Assert.assertEquals(pAD.provider, pD.andChildren.get(new Refinement(null, pA.provider)));
-
-            AndNode pAB = pA.andChildren.get(new Refinement(null, pB.provider)).child.get();
-            Assert.assertEquals(pAB.provider, pB.andChildren.get(new Refinement(null, pA.provider)));
-
-            AndNode pAC = pA.andChildren.get(new Refinement(null, pC.provider)).child.get();
-            Assert.assertEquals(pAC.provider, pC.andChildren.get(new Refinement(null, pA.provider)));
-
-            AndNode pBC = pB.andChildren.get(new Refinement(null, pC.provider)).child.get();
-            Assert.assertEquals(pBC.provider, pC.andChildren.get(new Refinement(null, pB.provider)));
-
-            AndNode pBD = pB.andChildren.get(new Refinement(null, pD.provider)).child.get();
-            Assert.assertEquals(pBD.provider, pD.andChildren.get(new Refinement(null, pB.provider)));
-
-            AndNode pCD = pC.andChildren.get(new Refinement(null, pD.provider)).child.get();
-            Assert.assertEquals(pCD.provider, pD.andChildren.get(new Refinement(null, pC.provider)));
-
-            AndNode pABC = pAB.andChildren.get(new Refinement(null, pC.provider)).child.get();
-            Assert.assertEquals(pABC.provider, pAC.andChildren.get(new Refinement(null, pB.provider)));
-            Assert.assertEquals(pABC.provider, pBC.andChildren.get(new Refinement(null, pA.provider)));
-
-            AndNode pABD = pAB.andChildren.get(new Refinement(null, pD.provider)).child.get();
-            Assert.assertEquals(pABD.provider, pAD.andChildren.get(new Refinement(null, pB.provider)));
-            Assert.assertEquals(pABD.provider, pBD.andChildren.get(new Refinement(null, pA.provider)));
-
-            AndNode pACD = pAC.andChildren.get(new Refinement(null, pD.provider)).child.get();
-            Assert.assertEquals(pACD.provider, pAD.andChildren.get(new Refinement(null, pC.provider)));
-            Assert.assertEquals(pACD.provider, pCD.andChildren.get(new Refinement(null, pA.provider)));
-
-            AndNode pBCD = pBC.andChildren.get(new Refinement(null, pD.provider)).child.get();
-            Assert.assertEquals(pBCD.provider, pBD.andChildren.get(new Refinement(null, pC.provider)));
-            Assert.assertEquals(pBCD.provider, pCD.andChildren.get(new Refinement(null, pB.provider)));
-
-            AndNode pABCD = pABC.andChildren.get(new Refinement(null, pD.provider)).child.get();
-            Assert.assertEquals(pABCD.provider, pABD.andChildren.get(new Refinement(null, pC.provider)));
-            Assert.assertEquals(pABCD.provider, pACD.andChildren.get(new Refinement(null, pB.provider)));
-            Assert.assertEquals(pABCD.provider, pBCD.andChildren.get(new Refinement(null, pA.provider)));
-
-            Assert.assertEquals(4, pABCD.parents.size());
-            Assert.assertEquals(null, pABCD.andChildren);
-        }
+        Assert.assertEquals(2, pABC.parents.size());
     }
 
 
