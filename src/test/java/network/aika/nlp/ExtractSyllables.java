37ab7d3644b375013a618657dd4484cdbbc1ebac
Lukas Molzberger <lukas.molzberger@meinestadt.de>
2018-05-13 10:54:14 +0200

    syllables extraction experiment

---
+++ src/test/java/network/aika/nlp/ExtractSyllables.java
@@ -0,0 +1,163 @@
+package network.aika.nlp;
+
+import network.aika.ActivationFunction;
+import network.aika.Document;
+import network.aika.Model;
+import network.aika.neuron.INeuron;
+import network.aika.neuron.Neuron;
+import network.aika.neuron.Synapse;
+import network.aika.training.MetaNetwork;
+import network.aika.training.MetaSynapse;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import static network.aika.neuron.activation.Range.Relation.BEGIN_TO_END_EQUALS;
+import static network.aika.neuron.activation.Range.Relation.OVERLAPS;
+
+public class ExtractSyllables {
+
+    public static String LETTERS = "abcdefghijklmnopqrstuvwxyzäöüß";
+
+    public Model model;
+
+    public Neuron documentN;
+    public Neuron letterInhib;
+    public Neuron syllableInhib;
+
+    public Map<Character, Neuron> inputLetters = new HashMap<>();
+
+
+    @Before
+    public void init() {
+        model = new Model();
+        documentN = model.createNeuron("DOCUMENT");
+
+        letterInhib = model.createNeuron("S-LETTER");
+        syllableInhib = model.createNeuron("S-SYLLABLE");
+
+        for(int i = 2; i < 6; i++) {
+            Neuron syllableMetaN = model.createNeuron("M-SYLLABLE-" + i);
+
+            ArrayList<Synapse.Builder> inputs = new ArrayList<>();
+            inputs.add(
+                    new MetaSynapse.Builder() // First letter of the pattern
+                            .setMetaWeight(2.0)
+                            .setMetaBias(-2.0)
+                            .setSynapseId(0)
+                            .setNeuron(letterInhib)
+                            .setWeight(10.0)
+                            .setBias(-10.0)
+                            .setRangeOutput(true, false)
+            );
+            for (int j = 0; j < i - 2; j++) {
+                inputs.add(
+                        new MetaSynapse.Builder() // Middle letter of the pattern
+                                .setMetaWeight(1.0)
+                                .setMetaBias(-1.0)
+                                .setSynapseId(j + 1)
+                                .setNeuron(letterInhib)
+                                .setWeight(5.0)
+                                .setBias(-5.0)
+                                .addRangeRelation(BEGIN_TO_END_EQUALS, j)
+                                .setRangeOutput(false)
+                );
+            }
+            inputs.add(
+                    new MetaSynapse.Builder() // Last letter of the pattern
+                            .setMetaWeight(2.0)
+                            .setMetaBias(-2.0)
+                            .setSynapseId(i - 1)
+                            .setNeuron(letterInhib)
+                            .setWeight(10.0)
+                            .setBias(-10.0)
+                            .addRangeRelation(BEGIN_TO_END_EQUALS, i - 2)
+                            .setRangeOutput(false, true)
+            );
+            inputs.add(
+                    new MetaSynapse.Builder()
+                            .setMetaWeight(-100.0)
+                            .setMetaBias(0.0)
+                            .setSynapseId(i)
+                            .setNeuron(syllableInhib)
+                            .setWeight(-100.0)
+                            .setBias(0.0)
+                            .setRecurrent(true)
+                            .addRangeRelation(OVERLAPS, Synapse.Builder.OUTPUT)
+            );
+
+            MetaNetwork.initMetaNeuron(syllableMetaN, 5.0, 2.0, inputs.toArray(new Synapse.Builder[inputs.size()]));
+
+            syllableInhib.addSynapse(
+                    new MetaSynapse.Builder()
+                            .setMetaWeight(1.0)
+                            .setMetaBias(0.0)
+                            .setNeuron(syllableMetaN)
+                            .setWeight(1.0)
+                            .setBias(0.0)
+                            .setRangeOutput(true)
+            );
+        }
+
+        Neuron.init(letterInhib, 0.0, ActivationFunction.RECTIFIED_LINEAR_UNIT, INeuron.Type.INHIBITORY);
+
+        Neuron.init(syllableInhib, 0.0, ActivationFunction.RECTIFIED_LINEAR_UNIT, INeuron.Type.INHIBITORY);
+
+        for (char c: LETTERS.toCharArray()) {
+            Neuron letterN = model.createNeuron("L-" + c);
+            inputLetters.put(c, letterN);
+
+            letterInhib.addSynapse(
+                    new Synapse.Builder()
+                            .setNeuron(letterN)
+                            .setWeight(1.0)
+                            .setBias(0.0)
+                            .setRangeOutput(true)
+            );
+        }
+    }
+
+
+    public void processCharacters(Document doc) {
+        for(int pos = 0; pos < doc.length(); pos++) {
+            char c = doc.getContent().charAt(pos);
+
+            Neuron inputLetter = inputLetters.get(Character.toLowerCase(c));
+            if(inputLetter != null) {
+                inputLetter.addInput(doc, pos, pos + 1);
+            }
+        }
+    }
+
+
+    @Test
+    public void extractSyllables() {
+        ArrayList<String> inputs = new ArrayList<>();
+        inputs.add("kuchen ");
+        inputs.add("küche ");
+        inputs.add("hoch ");
+        inputs.add("ich ");
+        inputs.add("chinesisch ");
+        inputs.add("mich ");
+        inputs.add("kochen ");
+        inputs.add("fluch ");
+        inputs.add("chef ");
+
+
+        for (String txt : inputs) {
+            System.out.println(txt);
+            Document doc = model.createDocument(txt);
+
+            processCharacters(doc);
+
+            doc.process();
+
+            MetaNetwork.train(doc);
+
+            doc.clearActivations();
+        }
+    }
+}
