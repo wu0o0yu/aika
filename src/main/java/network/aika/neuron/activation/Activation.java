8203cbb6f787c5c0d6b978b85957c069ec04d283
Lukas Molzberger <lukas.molzberger@meinestadt.de>
2018-08-16 12:15:13 +0200

    fix

--- src/main/java/network/aika/neuron/activation/Activation.java
+++ src/main/java/network/aika/neuron/activation/Activation.java
@@ -41,6 +41,8 @@ public final class Activation extends OrActivation {
     public static int MAX_SELF_REFERENCING_DEPTH = 5;
     public static int MAX_PREDECESSOR_DEPTH = 100;
 
+    public static Activation MIN_ACTIVATION = new Activation(Integer.MIN_VALUE, null, null);
+    public static Activation MAX_ACTIVATION = new Activation(Integer.MAX_VALUE, null, null);
 
     private static final Logger log = LoggerFactory.getLogger(Activation.class);
 
