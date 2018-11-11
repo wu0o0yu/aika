232b1bb5f3ebf05a5256fb2232ae1bbad805b2d2
Lukas Molzberger <lukas.molzberger@meinestadt.de>
2018-11-11 16:15:29 +0100

    work on range removal

--- src/main/java/network/aika/neuron/relation/MultiRelation.java
+++ src/main/java/network/aika/neuron/relation/MultiRelation.java
@@ -9,52 +9,80 @@ import network.aika.neuron.range.Position;
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 
 public class MultiRelation extends Relation {
+    public static final int RELATION_TYPE = 2;
+
+    private List<Relation> relations;
+
 
     public MultiRelation(List<Relation> rels) {
-
+        relations = rels;
     }
+
 
     @Override
     public int getRelationType() {
-        return 0;
+        return RELATION_TYPE;
     }
+
 
     @Override
     public boolean test(Activation act, Activation linkedAct) {
-        return false;
+        for(Relation rel: relations) {
+            if(!rel.test(act, linkedAct)) {
+                return false;
+            }
+        }
+
+        return true;
     }
+
 
     @Override
     public Relation invert() {
-        return null;
+        List<Relation> invRels = new ArrayList<>();
+        for(Relation rel: relations) {
+            invRels.add(rel.invert());
+        }
+        return new MultiRelation(invRels);
     }
+
 
     @Override
     public void mapRange(Map<Integer, Position> slots, Activation act) {
-
+        for(Relation rel: relations) {
+            rel.mapRange(slots, act);
+        }
     }
+
 
     @Override
     public void linksOutputs(Set<Integer> results) {
+        for(Relation rel: relations) {
+            rel.linksOutputs(results);
+        }
     }
 
 
     @Override
     public boolean isExact() {
+        for(Relation rel: relations) {
+            if(rel.isExact()) {
+                return true;
+            }
+        }
         return false;
     }
+
 
     @Override
     public Collection<Activation> getActivations(INeuron n, Activation linkedAct) {
         return null;
     }
+
 
     @Override
     public void registerRequiredSlots(Neuron input) {
