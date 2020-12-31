c8361026c8cf200071038ac4a485c468a5eea608
lukas <lukas.molzberger@gmx.de>
2020-12-31 16:03:30 +0100

    GradientTest

---
+++ src/test/java/network/GradientTest.java
@@ -0,0 +1,28 @@
+package network;
+
+import network.aika.text.Document;
+import network.aika.text.TextModel;
+import network.aika.text.TextReference;
+import org.junit.jupiter.api.Test;
+
+public class GradientTest {
+
+
+    @Test
+    public void gradientAndInduction() throws InterruptedException {
+        TextModel m = new TextModel();
+
+        Document doc = new Document("A B");
+
+        int i = 0;
+        TextReference lastRef = null;
+        for(String t: doc.getContent().split(" ")) {
+            int j = i + t.length();
+            lastRef = doc.processToken(m, lastRef, i, j, t).getReference();
+
+            i = j + 1;
+        }
+        doc.process(m);
+
+    }
+}
