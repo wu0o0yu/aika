69b3910a26b48285fe75acdcbf1f320ad5377d21
lukas <lukas.molzberger@gmx.de>
2021-12-16 14:45:38 +0100

    simplified test cases by introducing the class TestUtils

--- src/test/java/network/aika/OscillationTest.java
+++ src/test/java/network/aika/OscillationTest.java
@@ -2,54 +2,46 @@ package network.aika;
 
 import network.aika.debugger.AikaDebugger;
 import network.aika.neuron.Synapse;
+import network.aika.neuron.Templates;
 import network.aika.neuron.activation.Activation;
 import network.aika.neuron.excitatory.BindingNeuron;
 import network.aika.neuron.excitatory.PatternNeuron;
 import network.aika.text.Document;
 import network.aika.text.TextModel;
+import network.aika.utils.TestUtils;
 import org.junit.jupiter.api.Test;
+
+import static network.aika.utils.TestUtils.*;
 
 public class OscillationTest {
 
     @Test
     public void oscillationTest() {
         TextModel m = new TextModel();
+        Templates t = m.getTemplates();
 
         m.setN(912);
 
         Document doc = new Document(m, "A ");
         doc.setConfig(
-                new Config() {
-                    public String getLabel(Activation act) {
-                        return "X";
-                    }
-                }
+                getConfig()
                         .setAlpha(0.99)
                         .setLearnRate(-0.1)
                         .setEnableTraining(true)
         );
 
-        PatternNeuron nA = m.getTemplates().SAME_PATTERN_TEMPLATE.instantiateTemplate(true);
-        nA.setLabel("P-A");
+        PatternNeuron nA = createNeuron(t.SAME_PATTERN_TEMPLATE, "P-A");
 
         nA.setFrequency(53.0);
         nA.getSampleSpace().setN(299);
         nA.getSampleSpace().setLastPosition(899l);
 
-        BindingNeuron nPPA =  m.getTemplates().SAME_BINDING_TEMPLATE.instantiateTemplate(true);
-        nPPA.setLabel("B-A");
-
-        Synapse s = m.getTemplates().PRIMARY_INPUT_SYNAPSE_TEMPLATE.instantiateTemplate(nA, nPPA);
-
-        s.getWeight().setInitialValue(0.3);
-
-        s.linkInput();
-        s.linkOutput();
+        BindingNeuron nPPA = createNeuron(t.SAME_BINDING_TEMPLATE, "B-A");
+        createSynapse(t.PRIMARY_INPUT_SYNAPSE_TEMPLATE, nA, nPPA, 0.3);
 
         AikaDebugger.createAndShowGUI(doc);
 
         doc.addToken(nA, 0, 1);
-
         doc.process();
         doc.updateModel();
 
