652378ac1c16110e56ca092f9ec8bd394fbc25fb
Lukas Molzberger <lukas.molzberger@meinestadt.de>
2022-06-16 12:45:47 +0200

    work on the test case: "the black cat"

--- src/test/java/network/aika/TheBlackCatTest.java
+++ src/test/java/network/aika/TheBlackCatTest.java
@@ -47,7 +47,7 @@ public class TheBlackCatTest {
         coords.put(2, new double[]{-0.024, 0.53});
         coords.put(3, new double[]{0.301, 0.743});
         coords.put(4, new double[]{-0.167, 0.405});
-        coords.put(5, new double[]{0.112, 0.4});
+        coords.put(5, new double[]{0.112, 0.405});
         coords.put(6, new double[]{0.338, -0.008});
         coords.put(7, new double[]{0.336, 0.196});
         coords.put(8, new double[]{0.505, 0.871});
@@ -55,13 +55,13 @@ public class TheBlackCatTest {
         coords.put(10, new double[]{0.272, 0.405});
         coords.put(11, new double[]{0.505, 0.396});
         coords.put(12, new double[]{0.78, -0.023});
-        coords.put(13, new double[]{0.776, 0.177});
-        coords.put(14, new double[]{0.686, 0.532});
+        coords.put(13, new double[]{0.731, 0.188});
+        coords.put(14, new double[]{0.788, 0.519});
         coords.put(15, new double[]{0.901, 0.862});
-        coords.put(16, new double[]{0.686, 0.732});
-        coords.put(17, new double[]{0.901, 1.062});
-        coords.put(18, new double[]{0.628, 0.394});
-        coords.put(19, new double[]{0.964, 0.383});
+        coords.put(16, new double[]{0.621, 0.396});
+        coords.put(17, new double[]{1.149, 0.388});
+        coords.put(18, new double[]{0.023, 0.82});
+        coords.put(19, new double[]{0.476, 1.029});
 
         return coords;
     }
@@ -70,17 +70,18 @@ public class TheBlackCatTest {
         Map<Long, double[]> coords = new TreeMap<>();
 
         coords.put(1l, new double[]{2.087, 0.379});
-        coords.put(2l, new double[]{0.225, 0.877});
+        coords.put(2l, new double[]{0.201, 0.546});
         coords.put(3l, new double[]{3.554, 0.733});
         coords.put(4l, new double[]{0.628, -0.12});
         coords.put(5l, new double[]{1.62, -0.151});
-        coords.put(6l, new double[]{2.731, -0.173});
-        coords.put(7l, new double[]{0.636, 1.511});
-        coords.put(8l, new double[]{3.134, 1.505});
-        coords.put(9l, new double[]{1.29, 2.76});
+        coords.put(6l, new double[]{2.946, -0.166});
+        coords.put(7l, new double[]{0.653, 2.31});
+        coords.put(8l, new double[]{3.204, 2.28});
+        coords.put(9l, new double[]{1.651, 3.336});
         coords.put(10l, new double[]{1.632, 1.52});
-        coords.put(11l, new double[]{2.571, 1.491});
-        coords.put(12l, new double[]{2.324, 2.755});
+        coords.put(11l, new double[]{2.76, 1.513});
+        coords.put(12l, new double[]{2.17, 2.305});
+        coords.put(13l, new double[]{0.671, 3.305});
 
         return coords;
     }
@@ -118,10 +119,10 @@ public class TheBlackCatTest {
         BindingNeuron blackBN = createNeuron(t.BINDING_TEMPLATE, "black (black cat)");
         createSynapse(t.PRIMARY_INPUT_SYNAPSE_FROM_PATTERN_TEMPLATE, blackIN, blackBN, 10.0);
         BindingNeuron catBN2 = createNeuron(t.BINDING_TEMPLATE, "cat (black cat)");
-        createSynapse(t.PRIMARY_INPUT_SYNAPSE_FROM_PATTERN_TEMPLATE, catIN, catBN2, 10.0);
+        createSynapse(t.PRIMARY_INPUT_SYNAPSE_FROM_PATTERN_TEMPLATE, catIN, catBN2, 20.0);
 
-        createSynapse(t.RELATED_INPUT_SYNAPSE_TEMPLATE, m.getPreviousTokenRelationBindingNeuron(), catBN2, 10.0);
-        createSynapse(t.SAME_PATTERN_SYNAPSE_TEMPLATE, blackBN, catBN2, 10.0);
+        createSynapse(t.RELATED_INPUT_SYNAPSE_TEMPLATE, m.getPreviousTokenRelationBindingNeuron(), catBN2, 5.0);
+        createSynapse(t.SAME_PATTERN_SYNAPSE_TEMPLATE, blackBN, catBN2, 5.0);
 
         PatternNeuron blackCat = initPatternLoop(t, "black cat", blackBN, catBN2);
         updateBias(blackCat, 3.0);
